83d37739872d9befe2b6cbed283dcb517d0a7f37
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-09-02 21:19:13 +0200

    first commit - part 2: project 1: 10/16 generateTeamsForOneRound do not works yet full of errors

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamModel.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamModel.kt
@@ -1,1 +1,10 @@
 package jetbrains.kotlin.course.alias.team
+import jetbrains.kotlin.course.alias.util.Identifier
+data class Team(val id: Identifier, var points:Int = 0){
+    val name:String = "Team#${id + 1}"
+}
+
+//fun main() {
+//    val team = Team(0)
+//    println(team.name) // GameCard(id=3, capacity=5)
+//}
--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,26 @@
 package jetbrains.kotlin.course.alias.team
 
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
 @Service
-class TeamService {
+class TeamService(identifierFactory: IdentifierFactory = IdentifierFactory()) {
+    companion object {
+        var teamsStorage = mutableMapOf<Int, String>() // OK
+    }
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    /*Implement the generateTeamsForOneRound method.
+     The method must generate a list of teams and also store all of them into the teamsStorage map.
+     To create new teams, you need to use identifierFactory from the TeamService class to generate a new id.
+     We need to create this method to save game results for the leaderboard.
+
+     */
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        val teams = teamsStorage
+        teams.forEach {
+            teamsStorage.putIfAbsent(it.id, it.name))
+        }
+        return teams
+    }
 }
