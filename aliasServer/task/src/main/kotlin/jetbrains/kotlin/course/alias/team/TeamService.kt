20bf0836494848b46f3e014fe5a00189f511f700
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-09-03 14:05:25 +0200

    first commit - part 2: project 1: 10/16 generateTeamsForOneRound put class property into function inside the same class

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -4,23 +4,30 @@ import jetbrains.kotlin.course.alias.util.Identifier
 import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
+
 @Service
 class TeamService(identifierFactory: IdentifierFactory = IdentifierFactory()) {
     companion object {
-        var teamsStorage = mutableMapOf<Int, String>() // OK
+        var teamsStorage = mutableMapOf<Identifier, Team>()
     }
-
     /*Implement the generateTeamsForOneRound method.
      The method must generate a list of teams and also store all of them into the teamsStorage map.
      To create new teams, you need to use identifierFactory from the TeamService class to generate a new id.
      We need to create this method to save game results for the leaderboard.
 
      */
+    val createTeams= identifierFactory
+
     fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
-        val teams = teamsStorage
-        teams.forEach {
-            teamsStorage.putIfAbsent(it.id, it.name))
+        val listOfTeams = mutableListOf<Team>()
+        var abc = createTeams.uniqueIdentifier
+        for (teamId in createTeams){
+            listOfTeams.add(Team(teamId))
+            teamsStorage.putIfAbsent(teamId, Team(teamId))
+            println(Team(teamId))
+//            teamsStorage.putIfAbsent(IdentifierFactory(teamId), Team(teamId))
         }
-        return teams
+
+        return listOfTeams
     }
 }
